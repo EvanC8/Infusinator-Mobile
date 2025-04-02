02acbc4b64251a8e72c3ed19fb099a11f3358392
Evan Cedeno <penguin@Evans-MacBook-Pro-6.local>
2025-04-01 23:12:10 -0500

    Added essential project files

---
+++ InfusinatorMobile/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InfusinatorMobile
+//
+//  Created by Evan Cedeno on 4/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InfusinatorMobile/InfusinatorMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfusinatorMobileApp.swift
+//  InfusinatorMobile
+//
+//  Created by Evan Cedeno on 4/1/25.
+//
+
+import SwiftUI
+
+@main
+struct InfusinatorMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
